81546592e73a46ed996a428744de976c50d01a87
Thales Morais <thales.morais21@gmail.com>
2021-06-27 17:16:55 -0300

    Fixed last bugs

--- src/pages/Products/ListProducts/index.tsx
+++ src/pages/Products/ListProducts/index.tsx
@@ -5,7 +5,8 @@ import { error } from '@pnotify/core';
 import api from '../../../services/api';
 
 import { Container, Content, CardCustom, CardFooter } from './styles';
-import Layout from './../../layout';
+import Layout from '../../layout';
+
 interface Product {
   id: string;
   name: string;
@@ -51,62 +52,66 @@ const ListProducts: React.FC = () => {
 
   return (
     <>
-    <Layout>
-<Container>
-        <header>
-          <h1>Produtos</h1>
-          <button
-            type="button"
-            onClick={() => {
-              history.push('/products/new');
-            }}
-          >
-            NOVO PRODUTO
-          </button>
-        </header>
+      <Layout>
+        <Container>
+          <header>
+            <h1>Produtos</h1>
+            <button
+              type="button"
+              onClick={() => {
+                history.push('/products/new');
+              }}
+            >
+              NOVO PRODUTO
+            </button>
+          </header>
 
-        <Content>
-          {products.map(product => (
-            <CardCustom>
-              <img
-                src={
-                  product.images.length > 0
-                    ? product.images[0].image
-                    : 'https://lh3.googleusercontent.com/proxy/_rt_r6x_pktBfElOkUiKfrMj0QWsUClj7rarcNdcGaY0b_0h77mSBVWAP37GLpoa3FI7wNS7GFFS3Od-e6wc-V4IHPbAbjnndTC4ZJZMdkgDSXs4mtjSil08BMzVBIIbZwGgDJH2lA3n2A'
-                }
-                alt="image_product  "
-              />
-              <span>{product.name}</span>
-              <strong>
-                {Intl.NumberFormat('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                }).format(product.price)}
-              </strong>
+          <Content>
+            {products.map(product => (
+              <CardCustom>
+                <img
+                  src={
+                    product.images.length > 0
+                      ? product.images[0].image
+                      : 'https://lh3.googleusercontent.com/proxy/_rt_r6x_pktBfElOkUiKfrMj0QWsUClj7rarcNdcGaY0b_0h77mSBVWAP37GLpoa3FI7wNS7GFFS3Od-e6wc-V4IHPbAbjnndTC4ZJZMdkgDSXs4mtjSil08BMzVBIIbZwGgDJH2lA3n2A'
+                  }
+                  alt="image_product  "
+                />
+                <span>{product.name}</span>
+                <strong>
+                  {Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  }).format(product.price)}
+                </strong>
 
-              <CardFooter>
-                <button type="button">
-                  <FaEdit />
-                </button>
-                <button
-                  type="button"
-                  onClick={() => {
-                    if (
-                      window.confirm('Realmente deseja excluir este item ?')
-                    ) {
-                      handleDeleteProduct(product.id);
-                    }
-                  }}
-                >
-                  <FaTrash />
-                </button>
-              </CardFooter>
-            </CardCustom>
-          ))}
-        </Content>
-      </Container>
-    </Layout>
-      
+                <CardFooter>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      window.location.href = `/products/update/${product.id}`;
+                    }}
+                  >
+                    <FaEdit />
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      if (
+                        window.confirm('Realmente deseja excluir este item ?')
+                      ) {
+                        handleDeleteProduct(product.id);
+                      }
+                    }}
+                  >
+                    <FaTrash />
+                  </button>
+                </CardFooter>
+              </CardCustom>
+            ))}
+          </Content>
+        </Container>
+      </Layout>
     </>
   );
 };

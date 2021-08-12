4e9d2529eb8c0f9d461206b5943d6cc9942b66a2
Thales Morais de Almeida <thales@pop-os.localdomain>
2021-08-11 22:48:31 -0300

    Fix editor warning

--- src/pages/Products/FormProduct/index.tsx
+++ src/pages/Products/FormProduct/index.tsx
@@ -614,7 +614,7 @@ const FormProduct: React.FC<FormProps> = ({ product, functionAction }) => {
                   onEditorChange={handleEditorChange}
                   init={{
                     plugins:
-                      'advcode print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+                      'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
                     toolbar:
                       'code undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
                     autosave_ask_before_unload: true,

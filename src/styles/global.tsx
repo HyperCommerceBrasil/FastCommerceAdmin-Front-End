1cb9c6abab8d759677fe15aaa1e23c56a3e5a9ff
Thales Morais de Almeida <thales@pop-os.localdomain>
2021-08-11 21:18:16 -0300

    Adicionado confirmação da exclusão e exclusão da imagem quando novo produto, corrigido pequenos bugs

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -2,6 +2,8 @@ import { createGlobalStyle } from 'styled-components';
 import '@pnotify/core/dist/PNotify.css';
 
 import '@pnotify/core/dist/BrightTheme.css';
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
+
 
 export default createGlobalStyle`
   * {
